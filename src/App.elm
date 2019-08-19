4a3f0883f0f99b24697300752ee8283d749e2f66
Takanori Hoshi <takanori.hoshi@gmail.com>
2019-08-19 01:39:14 -0700

    Implement header and footer using elm-bootstrap

--- src/App.elm
+++ src/App.elm
@@ -1,7 +1,41 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Bootstrap.Grid as Grid
+import Html exposing (a, br, div, footer, h1, header, p, small, text)
+import Html.Attributes exposing (class, href)
 
 
 main =
-    text "Hello, Elm!"
+    div []
+        [ storeHeader
+        , storeFooter
+        ]
+
+
+storeHeader =
+    header [ class "py-4" ]
+        [ Grid.container [ class "text-center" ]
+            [ h1 []
+                [ a [ href "/" ]
+                    [ text "HoshiLe’s Store"
+                    ]
+                ]
+            ]
+        ]
+
+
+storeFooter =
+    footer [ class "py-4 bg-dark text-light" ]
+        [ Grid.container [ class "text-center" ]
+            [ p []
+                [ small []
+                    [ text "CSIS 3280 Project by Takanori Hoshi (300306402) and Ngoc Tin Le (300296440)"
+                    , br [] []
+                    , text "Design by "
+                    , a [ href "https://gihyo.jp/book/2018/978-4-297-10020-9" ]
+                        [ text "Bootstrap 4 Textbook of Frontend Development (Japanese)"
+                        ]
+                    ]
+                ]
+            ]
+        ]
